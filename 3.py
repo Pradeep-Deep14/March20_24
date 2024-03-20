68d04f0f9150e1772c3502694e6fc5b2563def29
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2024-03-20 10:12:51 +0530

    first commit

---
+++ 3.py
@@ -0,0 +1,12 @@
+user={"name": "Alice", "age": 30}
+name_check="Alice"
+
+if name_check in user:
+    print(f'Found {name_check}')
+else:
+    print(f'Did not find {name_check}')
+
+#Did not find Alice
+    
+#Here it looks for key so else block is executed.
+    
